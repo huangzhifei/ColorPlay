7b34f76c3cd8d148811c32d9c9f4a4b76faf5830
hzf <huangzhifei2009@126.com>
2015-11-30 00:44:15 +0800

    增加setting与about

---
+++ ColorPlay/Model/CPSettingData.h
@@ -0,0 +1,13 @@
+//
+//  CPSettingData.h
+//  ColorPlay
+//
+//  Created by huangzhifei on 15/11/27.
+//  Copyright © 2015年 huangzhifei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPSettingData : NSObject
+
+@end
---
+++ ColorPlay/Model/CPSettingData.m
@@ -0,0 +1,13 @@
+//
+//  CPSettingData.m
+//  ColorPlay
+//
+//  Created by huangzhifei on 15/11/27.
+//  Copyright © 2015年 huangzhifei. All rights reserved.
+//
+
+#import "CPSettingData.h"
+
+@implementation CPSettingData
+
+@end
